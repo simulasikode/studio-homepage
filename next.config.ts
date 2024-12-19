39cea287a2e3703834421523f634b9a03e2b4f5a
simulasikode <simulasikode@gmail.com>
2024-12-19 15:39:15 +0700

    update: add firebase hosting

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
